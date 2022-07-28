9a1252dc5429ff14e3f18ba31acffab2bf1a9708
aq032 <anthonyq032@gmail.com>
2022-07-27 22:31:45 -0700

    Added a 'createCard()' method to Engineer, Intern, and Manager classes. Added option for users to pick between a new engineer, intern, or completing team. Moved generatePage.js

--- index.js
+++ index.js
@@ -1,109 +1,157 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require('./dist/generatePage.js');
+const generatePage = require('./src/generatePage.js');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
 
-//List of questions the user is asked
-const questions = () => {
-  const userQuestions = inquirer.prompt([
-    {
-      type: 'input',
-      message: "MANAGER: What is your manager's name?",
-      name: 'managerName',
-    },
+//List of manager questions the user is asked
+const managerQuestions = [
+  {
+    type: 'input',
+    message: "MANAGER: What is your manager's name?",
+    name: 'managerName',
+  },
 
-    {
-      type: 'input',
-      message: "MANAGER: What is your manager's ID number?",
-      name: 'managerId',
-    },
+  {
+    type: 'input',
+    message: "MANAGER: What is your manager's ID number?",
+    name: 'managerId',
+  },
 
-    {
-      type: 'input',
-      message: "MANAGER: What is your manager's email?",
-      name: 'managerEmail',
-    },
+  {
+    type: 'input',
+    message: "MANAGER: What is your manager's email?",
+    name: 'managerEmail',
+  },
 
-    {
-      type: 'input',
-      message: "MANAGER: What is your manager's office number?",
-      name: 'managerNum',
-    },
+  {
+    type: 'input',
+    message: "MANAGER: What is your manager's office number?",
+    name: 'managerNum',
+  }
 
-    {
-      type: 'input',
-      message: "ENGINEER: What is your engineer's Name?",
-      name: 'engineerName',
-    },
+]
 
-    {
-      type: 'input',
-      message: "ENGINEER: What is your engineer's ID number?",
-      name: 'engineerId',
-    },
+//List of engineer questions the user is asked
+const engineerQuestions = [
+  {
+    type: 'input',
+    message: "ENGINEER: What is your engineer's Name?",
+    name: 'engineerName',
+  },
 
-    {
-      type: 'input',
-      message: "ENGINEER: What is your engineer's email?",
-      name: 'engineerEmail',
-    },
+  {
+    type: 'input',
+    message: "ENGINEER: What is your engineer's ID number?",
+    name: 'engineerId',
+  },
 
-    {
-      type: 'input',
-      message: "ENGINEER: What is your engineer's GitHub?",
-      name: 'engineerGithub',
-    },
+  {
+    type: 'input',
+    message: "ENGINEER: What is your engineer's email?",
+    name: 'engineerEmail',
+  },
 
-    {
-      type: 'input',
-      message: "INTERN: What is your intern's name?",
-      name: 'internName',
-    },
+  {
+    type: 'input',
+    message: "ENGINEER: What is your engineer's GitHub?",
+    name: 'engineerGithub',
+  }
+]
 
-    {
-      type: 'input',
-      message: "INTERN: What is your intern's ID number?",
-      name: 'internId',
-    },
+//List of intern questions the user is asked
+const internQuestions = [
+  {
+    type: 'input',
+    message: "INTERN: What is your intern's name?",
+    name: 'internName',
+  },
 
-    {
-      type: 'input',
-      message: "INTERN: What is your intern's email?",
-      name: 'internEmail',
-    },
+  {
+    type: 'input',
+    message: "INTERN: What is your intern's ID number?",
+    name: 'internId',
+  },
 
-    {
-      type: 'input',
-      message: "INTERN: What is your intern's school?",
-      name: 'internSchool',
-    }
-  ])
-  return userQuestions;
-}
+  {
+    type: 'input',
+    message: "INTERN: What is your intern's email?",
+    name: 'internEmail',
+  },
 
-//Writes info to new index.html files
+  {
+    type: 'input',
+    message: "INTERN: What is your intern's school?",
+    name: 'internSchool',
+  }
+]
+
+//Question asked at the end of every group of questions
+const continueQuestion = [
+  {
+    type: 'list',
+    message: "Would you like to add another member to your team?",
+    choices: ["Engineer", "Intern", "Finish team"],
+    name: 'goOrStop',
+  }
+]
+
+//Writes all retrieved data to 'index.html'
 function writeToFile(html) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile('./dist/index.html', html, err => {
-      if (err) {
-        console.log("Error writing HTML to file");
-        reject(err);
-        return;
-      }
-      console.log("index.html successfully generated!")
-      resolve();
-    })
+  fs.writeFile('./dist/index.html', html, err => {
+    if (err) {
+      console.log("Error writing HTML to file");
+      return;
+    }
+    console.log("index.html successfully generated!")
   })
 }
 
-//Questions() is called then executes a chain of events
-questions()
-.then(data => {
-  //Takes users input and runs the generatePage function with it
-  return generatePage(data)
-})
-.then(html => {
-  writeToFile(html);
-})
-.catch(err => {
-  console.log(err)
-})
+//Will be used to generate HTML for cards. Left as empty strings so they can still be used as parameters even if the user doesn't want an engineer or intern
+let managerHtml = '';
+let engineerHtml = '';
+let internHtml = '';
+
+//askQuestions() is called then executes a chain of events
+async function askQuestions() {
+  //Manager info
+  const managerInfo = await inquirer.prompt(managerQuestions);
+  const newManager = new Manager(managerInfo.managerName, managerInfo.managerId, managerInfo.managerEmail, managerInfo.managerNum);
+
+  //Creates a card with the manager's info and stores it within the 'managerHtml' variable
+  managerHtml = newManager.createCard();
+
+  //Going to make a while loop that continually runs until user clicks 'finish'
+  let keepRunning = true;
+  while (keepRunning) {
+    const newWorker = await inquirer.prompt(continueQuestion);
+    const workerPosition = newWorker.goOrStop;
+
+    //Will prompt user with engineer questions
+    if (workerPosition === "Engineer") {
+      //Creates a new Engineer based on the answers provided by the user
+      const engineerInfo = await inquirer.prompt(engineerQuestions);
+      const newEngineer = new Engineer(engineerInfo.engineerName, engineerInfo.engineerId, engineerInfo.engineerEmail, engineerInfo.engineerGithub)
+
+      engineerHtml = newEngineer.createCard();
+
+    // Will prompt user with intern questions  
+    } else if (workerPosition === "Intern") {
+      //Creates a new Intern based on the answers provided by the user
+      const internInfo = await inquirer.prompt(internQuestions);
+      const newIntern = new Intern(internInfo.internName, internInfo.internId, internInfo.internEmail, internInfo.internSchool)
+
+      internHtml = newIntern.createCard();
+    } else {
+      //When user selects 'Finish team', while loop ends
+      keepRunning = false;
+    }
+  }
+
+  const indexHtml = generatePage(managerHtml, engineerHtml, internHtml);
+
+  writeToFile(indexHtml);
+}
+
+askQuestions();
---
+++ src/generatePage.js
@@ -0,0 +1,34 @@
+const Manager = require('../lib/Manager.js');
+const Engineer = require('../lib/Engineer.js');
+const Intern = require('../lib/Intern.js');
+
+//Generates HTML Page writing
+function generatePage(manager, engineer, intern) {
+  //Returns completed text for index.html using the previously generated html for every
+  return `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My team!</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <link href="./style.css" rel="stylesheet">
+</head>
+
+<body>
+  <h1>My Team</h1>
+  
+  <section class="card-container">
+    ${manager}
+    ${engineer}
+    ${intern}
+  </section>
+</body>
+
+</html>
+`
+}
+
+module.exports = generatePage;
